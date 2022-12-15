2b468fac34236d294615115a03f11893cf9b9426
Winston Ametsitsi <winston@ametsitsi.com>
2022-12-14 21:33:18 -0800

    more testing

--- ember_mug/connection.py
+++ ember_mug/connection.py
@@ -169,6 +169,7 @@ class EmberMugConnection:
 
     async def get_meta(self) -> MugMeta:
         """Fetch Meta info from the mug (Serial number and ID)."""
+        logger.warning("hacking metdata and returning fake")
         return MugMeta.from_bytes(b'Yw====-ABCDEFGHIJ')
         # return MugMeta.from_bytes(await self._client.read_gatt_char(UUID_MUG_ID))
 
